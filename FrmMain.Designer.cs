7dc411c74c7bf18a6860f2375f65787a7192c827
iimax <imaxis@163.com>
2017-08-09 12:22:34 +0800

    ready to go

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace QPCommentWatcher
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnWatch = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 70);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(405, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(85, 21);
+            this.lblStatus.Text = "no comment";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnWatch);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(405, 70);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Watch";
+            // 
+            // btnWatch
+            // 
+            this.btnWatch.Location = new System.Drawing.Point(13, 31);
+            this.btnWatch.Name = "btnWatch";
+            this.btnWatch.Size = new System.Drawing.Size(75, 23);
+            this.btnWatch.TabIndex = 0;
+            this.btnWatch.Text = "Start";
+            this.btnWatch.UseVisualStyleBackColor = true;
+            this.btnWatch.Click += new System.EventHandler(this.btnWatch_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 96);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "QPCommentWatcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnWatch;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QPCommentWatcher
+{
+    public partial class FrmMain : Form
+    {
+        private static string folder = System.Configuration.ConfigurationManager.AppSettings["whichFolder"];
+        private FileSystemWatcher watcher = null;
+        public const string SublimeTextPath = "C:\\Program Files\\Sublime Text 3\\sublime_text.exe";
+        private string baseTitle = string.Empty;
+
+        [DllImport("user32.dll")]
+        internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        internal static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            baseTitle = this.Text;
+            if (string.IsNullOrWhiteSpace(folder) || !Directory.Exists(folder))
+            {
+                btnWatch.Enabled = false;
+                UpdateTitle("Disabled(Invalid folder)");
+            }
+        }
+
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            if (btnWatch.Enabled)
+            {
+                this.groupBox1.Text = string.Format("Wathing {0}", folder);
+                StartWatch();
+            }
+            
+        }
+
+        private void StartWatch()
+        {
+            Run(folder);
+            UpdateTitle("Running");
+            this.btnWatch.Text = "Stop";
+        }
+
+        private void btnWatch_Click(object sender, EventArgs e)
+        {
+            var action = this.btnWatch.Text;
+            switch (action)
+            {
+                case "Start":
+                    StartWatch();
+                    break;
+                default:
+                    Stop();
+                    break;
+            }
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Stop();
+        }
+
+        private void UpdateTitle(string msg)
+        {
+            this.Text = string.Format("{0} - {1}", baseTitle, msg);
+        }
+
+        private void UpdateStatus(string msg)
+        {
+            lblStatus.Text = msg;
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public void Run(string dir)
+        {
+            // Create a new FileSystemWatcher and set its properties.
+            watcher = new FileSystemWatcher();
+            watcher.Path = dir;
+            /* Watch for changes in LastAccess and LastWrite times, and
+               the renaming of files or directories. */
+            //watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            // Only watch text files.
+            watcher.Filter = "*.txt";
+
+            // Add event handlers.
+            //watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            //watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            //watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.IncludeSubdirectories = false;
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+
+        }
+
+        // Define the event handlers.
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            if (e.ChangeType == WatcherChangeTypes.Created)
+            {
+                try
+                {
+                    //Clipboard.SetText(e.Name);
+                    var fileName = Path.GetFileNameWithoutExtension(e.FullPath);
+                    SetClipboardText(fileName);
+                    UpdateStatus(fileName);
+                }
+                catch (Exception ex)
+                {
+                    UpdateStatus(ex.Message);
+                }
+                
+                Process.Start(SublimeTextPath, string.Format("\"{0}\"",e.FullPath));
+                
+            }
+            //Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+
+        public void Stop()
+        {
+            if (watcher != null)
+            {
+                watcher.EnableRaisingEvents = false;
+            }
+            btnWatch.Text = "Start";
+            UpdateTitle("Sopped");
+        }
+
+        private void SetClipboardText(string text)
+        {
+            //https://stackoverflow.com/questions/14082942/copy-result-to-clipboard
+            OpenClipboard(IntPtr.Zero);
+            var yourString = text;
+            var ptr = Marshal.StringToHGlobalUni(yourString);
+            SetClipboardData(13, ptr);
+            CloseClipboard();
+            Marshal.FreeHGlobal(ptr);
+        }
+
+    }
+}
